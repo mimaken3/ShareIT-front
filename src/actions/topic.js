a009a57208be15ef4053805e8b5fab0586544982
mima <ken0930mata@ezweb.ne.jp>
2020-04-18 08:02:47 +0900

    全トピック取得はtokenなしでも取得出来るように修正

--- src/actions/topic.js
+++ src/actions/topic.js
@@ -7,14 +7,10 @@ export const GET_ALL_TOPICS = "GET_ALL_TOPICS_EVENT";
 // redux-thunkを使えばそれが可能
 const ROOT_URL = "https://shareit-part2-pro.appspot.com";
 
-const shareIT_token = localStorage.getItem("shareIT_token");
+// const shareIT_token = localStorage.getItem("shareIT_token");
 
 // 全トピックを取得
 export const getAllTopics = () => async (dispatch) => {
-  const response = await axios.get(`${ROOT_URL}/api/topics`, {
-    headers: {
-      Authorization: "Bearer " + shareIT_token,
-    },
-  });
+  const response = await axios.get(`${ROOT_URL}/topics`);
   dispatch({ type: GET_ALL_TOPICS, response });
 };
