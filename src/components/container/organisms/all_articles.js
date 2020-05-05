40ba3730eb6033535b697bd35845e5a00da682fe
mima <ken0930mata@ezweb.ne.jp>
2020-05-05 14:59:13 +0900

    余計な記述を削除

--- src/components/container/organisms/all_articles.js
+++ src/components/container/organisms/all_articles.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
 import { reduxForm } from "redux-form";
-import Paging from "Atoms/paging";
 import Article from "Molecules/articles/article";
 import getLoginUserInfo from "Modules/getLoginUserInfo";
 
@@ -42,13 +41,9 @@ const mapStateToProps = (state) => {
   return {
     isEmpty: state.articles.is_empty,
     articles: state.articles.articles,
-    refPg: state.articles.ref_pg,
-    allPagingNum: state.articles.all_paging_num,
   };
 };
 
-// connect 第一引数はcomponentに渡すpropsを制御する
-// 第二引数はreducerを呼び出して、reduxで管理しているstateを更新する
 export default connect(
   mapStateToProps,
   mapDispatchToProps
