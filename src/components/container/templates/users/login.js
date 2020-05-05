9aef8191b4d884ab47952aaae7d0274ebadb0f83
mima <ken0930mata@ezweb.ne.jp>
2020-05-05 11:08:19 +0900

    余計なコメントアウトを削除

--- src/components/container/templates/users/login.js
+++ src/components/container/templates/users/login.js
@@ -18,17 +18,6 @@ class Login extends Component {
     dispatch: PropTypes.func.isRequired,
   };
 
-  // componentWillReceiveProps(nextProps) {
-  //   console.log("componentWillReceiveProps");
-  //   console.log("nextProps.session: " + nextProps.session);
-  //   if (nextProps.session) {
-  //     this.setState({ session: nextProps.session });
-  //     localStorage.setItem("session", this.state.session);
-  //   } else {
-  //     this.setState({ message: nextProps.message });
-  //   }
-  // }
-
   // ログイン
   async onSubmit(values) {
     const userInfo = {
@@ -135,26 +124,8 @@ const validate = (values) => {
 const mapDispatchToProps = { loginUserEvent };
 
 const mapStateToProps = "";
-// (state) => {
-// TODO: ログイン失敗時、入力していたフォームを表示?
-// };
 
-// stateとactionをcomponentに関連付ける実装
-// このstatusは状態のトップレベルを表す
-// ReduxのStoreを第一引数にとる関数で、Componentにpropsとして渡すものをフィルタリングするときに使う。
-// const mapStateToProps = "";
-
-// connect 第一引数はcomponentに渡すpropsを制御する
-// 第二引数はreducerを呼び出して、reduxで管理しているstateを更新する
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(
-  // enableReinitialize: When set to true, the form will reinitialize
-  // every time the initialValues prop change. Defaults to false.
-  // titleとbody属性を表示するときに使う
-  // 直接詳細画面へアクセスしたとき(本来なら最初に記事一覧を取得して、それらの情報がブラウザのメモリに残った状態で、
-  // 詳細へ行くとメモリから詳細を取得する)適宜、該当のイベントをAPIサーバから取得する
-  // formにはユニークな名前を渡す
-  reduxForm({ validate, form: "loginForm", enableReinitialize: true })(Login)
-);
+)(reduxForm({ validate, form: "loginForm" })(Login));
