d3bfcd854a56a3204195141bd4a38341c6c02d12
mima <ken0930mata@ezweb.ne.jp>
2020-05-23 12:50:52 +0900

    読込中が１０秒間表示されてた場合、リロードを促すようにした

--- src/components/container/templates/loading.js
+++ src/components/container/templates/loading.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CylinderSpinLoader } from "react-css-loaders";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
+import { Alert, AlertTitle } from "@material-ui/lab";
 
 // 読込中
 export default function Loading() {
-  return (
-    <Container component="main" maxWidth="sm">
-      <CssBaseline />
-      {/* 青にしたい場合は color="#00CCFF" */}
-      <CylinderSpinLoader size={18} />
-    </Container>
-  );
+  const [timeOut, setTimeOut] = useState(false);
+
+  useEffect(() => {
+    // １０秒間 読込中の場合、リロードを促す
+    const timer = setTimeout(() => {
+      setTimeOut(true);
+    }, 10000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (timeOut) {
+    return (
+      <Container component="main" maxWidth="sm">
+        <CssBaseline />
+        <div style={{ marginTop: "0px" }}>
+          <Alert severity="warning">
+            <AlertTitle>読み込みに失敗</AlertTitle>
+            再読み込みをして下さい
+          </Alert>
+        </div>
+      </Container>
+    );
+  } else {
+    return (
+      <Container component="main" maxWidth="sm">
+        <CssBaseline />
+        {/* 青にしたい場合は color="#00CCFF" */}
+        <CylinderSpinLoader size={18} />
+      </Container>
+    );
+  }
 }
