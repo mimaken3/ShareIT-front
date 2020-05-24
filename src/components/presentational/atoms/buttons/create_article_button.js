b81692cf44229868b7762f904a6228658b1ebc16
mima <ken0930mata@ezweb.ne.jp>
2020-05-24 15:38:51 +0900

    投稿するページに遷移したとき、一番上にいるように修正

--- src/components/presentational/atoms/buttons/create_article_button.js
+++ src/components/presentational/atoms/buttons/create_article_button.js
@@ -2,18 +2,26 @@ import React from "react";
 import { withRouter } from "react-router";
 import Button from "@material-ui/core/Button";
 import getLoginUserInfo from "Modules/getLoginUserInfo";
+import { ScrollTo } from "react-scroll-to";
 
 // 投稿ボタン
 const CreateArticleButton = withRouter((props) => {
   return (
     <React.Fragment>
-      <Button
-        style={{ color: "white", fontSize: "18px", marginRight: "10px" }}
-        variant="outlined"
-        onClick={() => handlePost()}
-      >
-        投稿
-      </Button>
+      <ScrollTo>
+        {({ scroll }) => (
+          <Button
+            style={{ color: "white", fontSize: "18px", marginRight: "10px" }}
+            variant="outlined"
+            onClick={() => {
+              scroll({ x: 0, y: 0 });
+              handlePost();
+            }}
+          >
+            投稿
+          </Button>
+        )}
+      </ScrollTo>
     </React.Fragment>
   );
   function handlePost() {
