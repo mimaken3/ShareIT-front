469b0f517246fdba7d406e5675406307c32a6166
mima <ken0930mata@ezweb.ne.jp>
2020-05-27 19:02:01 +0900

    再読み込みで404になるバグを修正

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const path = require("path");
 
 const app = express();
 
-// Setup view engine
-app.set("view engine", "html");
-
-app.use(express.static(path.resolve(path.join(__dirname, "/public"))));
 app.use(express.static(path.resolve(path.join(__dirname, "/dist"))));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
